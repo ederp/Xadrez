e09d15f9c9dbdfce85c76764228c956c94682641
ederp <eder.pereira@aluno.ifsp.edu.br>
2020-04-08 15:27:30 -0300

    Fazendo os primeiros testes de movimentacao

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
+
 namespace Xadrez {
     class Tela {
 
         public static void imprimirTabuleiro(Tabuleiro tab) {
             for (int i = 0; i < tab.linhas; i++) {
-                System.Console.Write(8 - i + " ");
+                System.Console.Write(8 - i +" ");
                 for (int j = 0; j < tab.colunas; j++) {
                     if (tab.peca(i,j) == null) {
                         System.Console.Write("- ");
@@ -19,14 +21,21 @@ namespace Xadrez {
             System.Console.WriteLine("  a b c d e f g h");
         }
 
+        public static PosicaoXadrez lerPosicaoXadrez() {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1]+ " ");
+            return new PosicaoXadrez(coluna, linha);
+        }
+
         public static void imprimirPeca(Peca peca) {
             if(peca.cor == Cor.Branca) {
-                System.Console.Write(peca);
+                System.Console.Write(peca+" ");
             }
             else {
                 ConsoleColor aux = Console.ForegroundColor;
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write(peca);
+                Console.Write(peca+" ");
                 Console.ForegroundColor = aux;
             }
         }
